f81fc9e545d8f098734dd81700e7427f8b248f58
Shahab Nawaz <shahab.nawaz@kalp.studio>
2025-08-10 09:47:02 +0000

    Add Selenium test file

---
+++ src/test/java/Seleniumtest.java
@@ -0,0 +1,30 @@
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(AllureJunit5.class) // class level pe laga do
+public class Seleniumtest {
+
+    @Test  // method ko test declare karo
+    public void googleSearchTest() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("Shahab");
+        driver.findElement(By.name("q")).submit();
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.titleContains("Shahab"));
+
+        assertTrue(driver.getTitle().contains("Shahab"));
+        driver.quit();
+    }
+}
