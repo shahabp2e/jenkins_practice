bd807c49029ec81562a975d6ff402f6f75fd228c
shahabp2e <shahab.nawaz@kalp.studio>
2025-08-12 11:01:09 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -5,27 +5,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(AllureJunit5.class) // class level pe laga do
+@ExtendWith(AllureJunit5.class)
 public class SeleniumTest {
 
-    @Test  // method ko test declare karo
+    @Test
     public void googleSearchTest() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+        // Chrome options for Jenkins/CI environment
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Headless mode
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.google.com");
         driver.findElement(By.name("q")).sendKeys("Shahab");
         driver.findElement(By.name("q")).submit();
+
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.titleContains("Shahab"));
 
         assertTrue(driver.getTitle().contains("Shahab"));
+
         driver.quit();
     }
 }
-
