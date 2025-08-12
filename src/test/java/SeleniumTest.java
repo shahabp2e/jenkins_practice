675367c0e128d589307282b5eeef20cbc88498c7
shahabp2e <shahab.nawaz@kalp.studio>
2025-08-12 15:47:55 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -20,7 +20,7 @@ public class SeleniumTest {
     @BeforeEach
     public void setUp() throws IOException {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+//        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
